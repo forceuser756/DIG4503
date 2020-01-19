a2f3442df40d8f0350b784c1ca2eff6b5eee6db6
forceuser756 <42787700+forceuser756@users.noreply.github.com>
2020-01-18 21:58:18 -0500

    Lab 1 done

---
+++ Lab1/Trevor.js
@@ -0,0 +1,7 @@
+const Person = require('./Person.js');
+class Trevor extends Person {
+  constructor(name, favoriteColor){
+    super(name, favoriteColor);
+  }
+}
+module.exports = Trevor;
---
+++ Lab1/index.js
@@ -0,0 +1,5 @@
+const Trevor = require("./Trevor.js");
+
+let info = new Trevor("Trevor", "red");
+
+info.speak();
